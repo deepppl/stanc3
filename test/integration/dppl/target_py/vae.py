9682921acadf95655fd4f80dc13c105cb988a742
Louis Mandel <lmandel@us.ibm.com>
2020-11-02 14:24:15 -0500

    Update test vae

--- test/integration/dppl/target_py/vae.py
+++ test/integration/dppl/target_py/vae.py
@@ -1,21 +1,23 @@
+from runtimes.pyro.distributions import *
+from runtimes.pyro.dppllib import sample, param, observe, factor, array, zeros, ones, matmul, dtype_long, dtype_double, register_network
 
-import torch
-from torch import tensor, rand
-import pyro
-import torch.distributions.constraints as constraints
-import pyro.distributions as dist
-
-def guide_(nz=None, x=None):
-    pyro.module('encoder', encoder)
-    encoded = encoder(x)
-    mu_z = encoded[(1 - 1)]
-    sigma_z = encoded[(2 - 1)]
-    z = sample('z', dist.Normal(mu_z, sigma_z))
-
-def model(nz=None, x=None):
-    pyro.module('decoder', decoder)
-    z = sample('z', ImproperUniform(shape=nz))
-    mu = zeros((28, 28))
-    sample(('z' + '__1'), dist.Normal(zeros(nz), 1), obs=z)
+def model(*, nz, x, decoder, encoder):
+    # Networks
+    register_network('decoder', decoder)
+    # Parameters
+    z = sample('z', improper_uniform(shape=[nz]))
+    # Model
+    mu = zeros([28, 28])
+    observe('z__1', normal(0, 1), z)
     mu = decoder(z)
-    sample(('x' + '__2'), dist.Bernoulli(mu), obs=x)
+    for i in range(1,28 + 1):
+        observe(f'x__{i}__2', bernoulli(mu[i - 1]), x[i - 1])
+
+def guide(*, nz, x, decoder, encoder):
+    # Networks
+    register_network('encoder', encoder)
+    # Guide
+    encoded = encoder(x)
+    mu_z = encoded[1 - 1]
+    sigma_z = encoded[2 - 1]
+    z = sample('z', normal(mu_z, sigma_z))
