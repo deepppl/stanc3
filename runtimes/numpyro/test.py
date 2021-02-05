072472fb8b7b512eab58687ccd88ad32352e728c
Louis Mandel <lmandel@us.ibm.com>
2021-02-05 17:40:46 -0500

    The runtimes moved to https://github.com/deepppl/stan-num-pyro

--- runtimes/numpyro/test.py
+++
@@ -1,8 +0,0 @@
-from dppl import NumpyroModel
-from numpy import array
-
-test = NumpyroModel("/tmp/a.stan")
-mcmc = test.mcmc(warmups=10, samples=100)
-mcmc.run(N=10, x=array([0 for _ in range(10)]))
-
-print(mcmc.get_samples())
